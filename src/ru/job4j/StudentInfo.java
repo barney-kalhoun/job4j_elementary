b8b0d1101f9cecd57abe237411ac835096bf8023
barney-kalhoun <email@example.com>
2021-08-09 23:08:04 +0700

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("User Name");
+        System.out.println("99.99.2099");
+    }
+}
