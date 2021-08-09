4a22d901f142764f08b82275a3a71b4e0c264ac7
barney-kalhoun <email@example.com>
2021-08-09 23:08:49 +0700

    Revert "3.3. Разбор первой программы. [#159033]"
    This reverts commit b8b0d110

--- src/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("User Name");
-        System.out.println("99.99.2099");
-    }
-}
