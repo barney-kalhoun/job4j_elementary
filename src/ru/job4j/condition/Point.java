2b818a23628f40336e895edecc061e9e91c57340
barney-kalhoun <email@example.com>
2021-08-13 02:49:25 +0700

    9.2. Checkstyle [#4123]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -6,12 +6,12 @@ public class Point {
         double first = x2 - x1;
         double second = y2 - y1;
 
-        double rsl = Math.sqrt(Math.pow(first,2) + Math.pow(second,2));
+        double rsl = Math.sqrt(Math.pow(first, 2) + Math.pow(second, 2));
         return rsl;
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0,0,2,0);
+        double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0,0) to (2,0) " + result);
     }
 }
