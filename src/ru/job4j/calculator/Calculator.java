59efc94b70ca0fc0dc6c7137bf0e5ff3e72ca155
barney-kalhoun <email@example.com>
2021-08-14 16:33:17 +0700

    1.1. Что такое import [#504773]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,29 @@
 package ru.job4j.calculator;
 
+import static ru.job4j.math.MathFunction.*;
+
 public class Calculator {
 
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double sumOfSubtractionAndDivison(double first, double second) {
+        return subtract(first, second)
+                + divide(first, second);
+    }
+
+    public static double sumOfAllOperations(double first, double second) {
+        return sum(first, second)
+                + subtract(first, second)
+                + multiply(first, second)
+                + divide(first, second);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-
-        int six = 6;
-        int four = 4;
-        int five = 5;
-
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
-
+        System.out.println("Результат 1 расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат 2 расчета равен: " + sumOfSubtractionAndDivison(10, 20));
+        System.out.println("Результат 3 расчета равен: " + sumOfAllOperations(10, 20));
     }
 }
