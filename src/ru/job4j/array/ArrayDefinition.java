5b1d99db4eb6d94ea8a7876249ad870d9781b214
barney-kalhoun <email@example.com>
2021-08-20 17:08:17 +0700

    Revert "6.0.3. Массивы и цикл for. [#226665]"
    This reverts commit 97eda91b

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,14 +4,12 @@ import java.util.Arrays;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        String[] names = new String[4];
-        names[0] = "Frodo";
-        names[1] = "Sam";
-        names[2] = "Merry";
-        names[3] = "Pippin";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+
+        System.out.println("Размер массива ages равен: " + ages.length);
+        System.out.println("Размер массива surnames равен: " + surnames.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
     }
 }
